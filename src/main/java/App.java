70208802cc854c1c6eef5f68c55c998a48e07a91
ajju03 <ajayprasanna7@gmail.com>
2025-11-10 15:58:56 +0530

    App.java is edited in order to run the contianer persistently

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,6 +1,19 @@
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+import com.sun.net.httpserver.HttpServer;
+
 public class App {
-    public static void main(String[] args) {
-        System.out.println("Hello, Jenkins CI/CD World!");
+    public static void main(String[] args) throws IOException {
+        HttpServer server = HttpServer.create(new InetSocketAddress(8080), 0);
+        server.createContext("/", exchange -> {
+            String response = "Hello, Jenkins CI/CD World!";
+            exchange.sendResponseHeaders(200, response.length());
+            try (OutputStream os = exchange.getResponseBody()) {
+                os.write(response.getBytes());
+            }
+        });
+        server.start();
+        System.out.println("🚀 Server running on port 8080...");
     }
 }
-
